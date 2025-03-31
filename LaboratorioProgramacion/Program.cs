67fe6897ac47114b735e88f5487a83cb6c732e45
Mariano Lopez <marianoalopez99@gmail.com>
2025-03-31 11:51:41 -0300

    Actualizacion de mensajes.

--- LaboratorioProgramacion/Program.cs
+++ LaboratorioProgramacion/Program.cs
@@ -1,4 +1,4 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
-
+Console.WriteLine("Lindo lunes para estar con Visual Studio");
+Console.WriteLine("¡Primer cambio en el código, subiéndo a GitHub!");
 
