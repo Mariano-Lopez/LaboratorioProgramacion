1035ba677968a9700fdfd868716090e935884ef8
Mariano Lopez <marianoalopez99@gmail.com>
2025-03-31 12:13:27 -0300

    Seguno mensaje de cambio.

--- LaboratorioProgramacion/Program.cs
+++ LaboratorioProgramacion/Program.cs
@@ -1,4 +1,5 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Lindo lunes para estar con Visual Studio");
 Console.WriteLine("¡Primer cambio en el código, subiéndo a GitHub!");
+Console.WriteLine("¡Segundo cambio en el código, subiéndo a GitHub!");
 
